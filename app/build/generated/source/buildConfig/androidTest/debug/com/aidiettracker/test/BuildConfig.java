04660e6eb1ad7d5166612910faac77cc3d90b419
Akshat Prakash Kanth <110756105+Akshatkanth@users.noreply.github.com>
2026-04-07 20:09:37 +0530

    login logic fixed

---
+++ app/build/generated/source/buildConfig/androidTest/debug/com/aidiettracker/test/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.aidiettracker.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.aidiettracker.test";
+  public static final String BUILD_TYPE = "debug";
+  // Field from default config.
+  public static final String NIM_API_KEY = "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";
+  // Field from default config.
+  public static final String NIM_BASE_URL = "https://integrate.api.nvidia.com/";
+  // Field from default config.
+  public static final String NIM_MODEL = "meta/llama-3.1-8b-instruct";
+}
